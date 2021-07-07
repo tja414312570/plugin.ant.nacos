39dcae646351d636e92cc1b5b68f9447174ca645
Plugin <tja414312570@gmail.com>
2021-07-08 00:30:06 +0800

    111

--- src/test/java/com/YaNan/frame/ant/test/DiscoveryServiceTest.java
+++ src/test/java/com/YaNan/frame/ant/test/DiscoveryServiceTest.java
@@ -1,23 +1,29 @@
 package com.YaNan.frame.ant.test;
 
-import com.yanan.framework.plugin.*;
-import com.yanan.utils.resource.*;
-import com.yanan.framework.ant.*;
-import com.yanan.framework.ant.nacos.AntNacosConfigureFactory;
-import com.yanan.framework.plugin.decoder.*;
-import com.yanan.framework.a.core.cluster.*;
-import com.yanan.framework.a.core.server.ServerMessageChannel;
-import com.yanan.framework.a.nacos.NacosConfigureFactory;
-
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.util.Properties;
 
-import com.yanan.framework.a.core.*;
-import com.yanan.framework.plugin.event.*;
+import com.yanan.framework.a.core.MessageChannel;
+import com.yanan.framework.a.core.cluster.ChannelManager;
+import com.yanan.framework.a.nacos.NacosConfigureFactory;
+import com.yanan.framework.ant.nacos.AntNacosConfigureFactory;
+import com.yanan.framework.plugin.Environment;
+import com.yanan.framework.plugin.PluginEvent;
+import com.yanan.framework.plugin.PluginEvent.EventType;
+import com.yanan.framework.plugin.PlugsFactory;
+import com.yanan.framework.plugin.decoder.StandScanResource;
+import com.yanan.framework.plugin.definition.RegisterDefinition;
+import com.yanan.utils.resource.ResourceManager;
 
 public class DiscoveryServiceTest {
 	public static void main(final String[] args) throws InterruptedException, IOException {
 
+		Environment.getEnviroment().registEventListener(PlugsFactory.getInstance().getEventSource(),event->{
+    		if( ((PluginEvent)event).getEventType() == EventType.add_registerDefinition)
+			System.err.println(((RegisterDefinition)((PluginEvent)event).getEventContent()).getRegisterClass());
+		});
+		
 		PlugsFactory.init(ResourceManager.getResource("classpath:plugin.yc"),
 				new StandScanResource(ResourceManager.getClassPath(MessageChannel.class)[0] + "**"),
 				new StandScanResource(ResourceManager.getClassPath(AntNacosConfigureFactory.class)[0] + "**"),
