b8667e037ee3a2ad99a16c18d7399298b98457cf
Plugin <tja414312570@gmail.com>
2021-07-16 16:01:16 +0800

    测试代码

--- src/test/java/ant/test/Request.java
+++ src/test/java/ant/test/Request.java
@@ -6,4 +6,7 @@ import com.yanan.framework.a.proxy.Ant;
 public interface Request
 {
     int add(final int p0, final int p1);
+
+	int multi(int i, int j);
+
 }
--- src/test/java/ant/test/Test.java
+++ src/test/java/ant/test/Test.java
@@ -8,5 +8,13 @@ public class Test implements Request{
 	public int add(int p0, int p1) {
 		return p0+p1;
 	}
+
+	@Override
+	public int multi(int i, int j) {
+		return i*j;
+	}
 	
+	public static void main(String[] args) {
+		System.err.println(String.valueOf(null));
+	}
 }
