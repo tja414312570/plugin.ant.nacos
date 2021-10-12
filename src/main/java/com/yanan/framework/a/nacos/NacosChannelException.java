1456fe18a66622ca930352b7202298cf7d0ae6c4
Plugin <tja414312570@gmail.com>
2021-10-12 17:47:33 +0800

    for dreams

---
+++ src/main/java/com/yanan/framework/a/nacos/NacosChannelException.java
@@ -0,0 +1,14 @@
+package com.yanan.framework.a.nacos;
+
+public class NacosChannelException extends RuntimeException {
+
+	public NacosChannelException(String msg, Throwable e) {
+		super(msg,e);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7570448869217299497L;
+
+}
--- src/test/java/com/YaNan/frame/ant/test/DiscoveryServiceServerTest.java
+++ src/test/java/com/YaNan/frame/ant/test/DiscoveryServiceServerTest.java
@@ -1,19 +1,21 @@
 package com.YaNan.frame.ant.test;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.Properties;
 
-import com.yanan.framework.a.channel.socket.server.MessageChannelCreateListener;
+import com.YaNan.test.ant.Provider;
 import com.yanan.framework.a.core.MessageChannel;
 import com.yanan.framework.a.core.cluster.ChannelManager;
 import com.yanan.framework.a.core.server.ServerMessageChannel;
 import com.yanan.framework.a.dispatcher.ChannelDispatcher;
 import com.yanan.framework.a.nacos.NacosConfigureFactory;
-import com.yanan.framework.a.proxy.Invoker;
-import com.yanan.framework.ant.nacos.AntNacosConfigureFactory;
+import com.yanan.framework.a.nacos.NacosInstance;
+import com.yanan.framework.a.proxy.Invokers;
 import com.yanan.framework.plugin.Environment;
 import com.yanan.framework.plugin.PlugsFactory;
 import com.yanan.framework.plugin.decoder.StandScanResource;
+import com.yanan.utils.reflect.cache.ClassHelper;
 import com.yanan.utils.resource.ResourceManager;
 
 public class DiscoveryServiceServerTest
@@ -29,23 +31,37 @@ public class DiscoveryServiceServerTest
 //        System.setProperty("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
         PlugsFactory.init(ResourceManager.getResource("classpath:plugin.yc"), 
         		new StandScanResource(ResourceManager.getClassPath(MessageChannel.class)[0] + "**"), 
-        new StandScanResource(ResourceManager.getClassPath(AntNacosConfigureFactory.class)[0] + "**"), 
+        		new StandScanResource(ResourceManager.getClassPath(NacosInstance.class)[0] + "**"), 
         new StandScanResource(ResourceManager.getClassPath(DiscoveryServiceServerTest.class)[0] + "**") );
         
         Properties properties = NacosConfigureFactory.build("classpath:ant.yc");
         
-        ChannelManager<?> server = PlugsFactory.getPluginsInstance(ChannelManager.class,properties);
+        ChannelManager<Object> channelManager = PlugsFactory.getPluginsInstance(ChannelManager.class,properties);
         
         ServerMessageChannel<String> client = PlugsFactory.getPluginsInstance(ServerMessageChannel.class);
         
-        server.start(client);
-        ChannelDispatcher<Object> channelDispatcher = PlugsFactory.getPluginsInstance(ChannelDispatcher.class);
+        channelManager.start(client);
+//        channelManager.start();
+        ChannelDispatcher channelDispatcher = PlugsFactory.getPluginsInstance(ChannelDispatcher.class);
 		System.err.println(channelDispatcher);
-		channelDispatcher.bind(server);
+		channelDispatcher.bind(channelManager);
 		
-		Invoker invoker = PlugsFactory.getPluginsInstance(Invoker.class);
 		
-		channelDispatcher.bind(invoker);
+//		Invoker invoker = PlugsFactory.getPluginsInstance(Invoker.class);
+//		
+//		channelDispatcher.bind(invoker);
+		
+		
+		Invokers invokers = new Invokers();
+		invokers.setInvokeClass(Provider.class);
+		Method method = ClassHelper.getClassHelper(Provider.class).getMethod("add", int.class,int.class);
+		invokers.setInvokeMethod(method);
+		invokers.setInvokeParmeters(1,2);
+		NacosInstance nacosInstance = new NacosInstance();
+		nacosInstance.setName("defaultName");
+		Object result = channelDispatcher.request("defaultName", invokers);
+		System.out.println("执行结果:"+result);
+		System.out.println("服务列表:"+channelManager.getChannelList("defaultName"));
 //        final MessageChannel<?> messageChannel = (MessageChannel<?>)server.getChannel("default");
 //		System.out.println(messageChannel);
 	}
